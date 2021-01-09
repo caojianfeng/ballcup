a58a089ea501a41bdb33d57d6715a6e1cda16789
JeffreyCao <jeffreycao1024@gmail.com>
2021-01-09 20:56:29 +0800

    add engin

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// 添加游戏引擎1/3
+import { GameEngine } from "react-native-game-engine";
 
 export default function App() {
+  // 添加游戏引擎1/3
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GameEngine style={styles.container}>
+      <StatusBar hidden={true} />
+    </GameEngine>
   );
 }
 
@@ -15,7 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
